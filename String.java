90981fb620009dfdc10754a32689b4597fdc43ae
Arulpriya-7 <123655330+Arulpriya-7@users.noreply.github.com>
2023-03-16 11:18:51 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int n=stones.length();
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(jewels.indexOf(stones.charAt(i))>-1)
+            c++;
+        }
+        return c;
+        
+    }
+}
