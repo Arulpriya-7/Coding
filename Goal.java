f6455f57c3df571bd5bd9c6baaba645ca7223a1d
Arulpriya-7 <123655330+Arulpriya-7@users.noreply.github.com>
2023-03-16 11:39:22 +0530

    Create Goal.java

---
+++ Goal.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        int n=command.length();
+        for(int i=0;i<n;i++)
+        {
+            if(command.charAt(i)=='G'){
+            sb.append("G");
+        }
+        else if(command.charAt(i)=='(')
+        {
+            if(command.charAt(i+1)==')')
+            {
+                sb.append("o");
+                i++;
+            }
+            else
+            {
+                sb.append("al");
+                i=i+3;
+            }
+        }
+        }
+        return sb.toString();
+       
+    }
+}
